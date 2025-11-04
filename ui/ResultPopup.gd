4c120dd2feaab5f2af50bca02d09fb976557067f
afftor <afftor2@gmail.com>
2025-11-04 04:51:47 +0300

    Revert "Add 2D Godot hex-field skirmish prototype"

--- ui/ResultPopup.gd
+++
@@ -1,9 +0,0 @@
-extends PopupDialog
-
-func _ready() -> void:
-    var button := $VBoxContainer/RestartButton
-    if button:
-        button.connect("pressed", self, "_on_restart_pressed")
-
-func _on_restart_pressed() -> void:
-    get_tree().reload_current_scene()
